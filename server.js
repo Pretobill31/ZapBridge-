832ce34368f8590ffd7e06c2a789ed8e2e15f4ff
Pretobill31 <billalves907@gmail.com>
2026-05-08 17:19:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post("/webhook", async (req, res) => {
 
     // CHAMADA GEMINI
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
@@ -67,7 +67,7 @@ app.post("/webhook", async (req, res) => {
 
     }
 
-    // SUCESSO
+    // RESPOSTA FINAL
     res.json({
       status: "ok",
       resposta
